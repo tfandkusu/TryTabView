ed3914b14bab102b649d9443b93cbab31a5c8c57
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-08-05 22:41:39 +0900

    Initial Commit

---
+++ TryTabView/TryTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryTabViewApp.swift
+//  TryTabView
+//
+//  Created by toya on 2023/08/05.
+//
+
+import SwiftUI
+
+@main
+struct TryTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
