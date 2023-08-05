7bae88bb2f87aa808159ec3ca5e137b6ab86c07c
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-08-05 23:48:05 +0900

    YearMonth struct を作成

---
+++ TryTabView/MainPageView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct MainPageView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct MainPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainPageView()
+    }
+}
---
+++ TryTabView/Model/YearMonth.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct YearMonth : Equatable {
+    var year: Int
+    var month: Int
+    
+    func previous() -> YearMonth {
+        if(month >= 2) {
+            return YearMonth(year: self.year, month: self.month - 1)
+        }else {
+            return YearMonth(year: self.year - 1, month: 12)
+        }
+    }
+}
