a8da1f9bcce6bb22e618c39fbd8796a2c0d0f154
Haruhiko Takada <haruhiko.takada@gmail.com>
2023-08-05 22:54:43 +0900

    最初の画面をMainViewに名前変更

--- TryTabView/MainView.swift
+++ TryTabView/MainView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -12,8 +12,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct MainView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        MainView()
     }
 }
